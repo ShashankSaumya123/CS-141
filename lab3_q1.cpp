105859f30b0ef1aa032b510432b172956aaa89d1
ShashankSaumya123 <42140139+ShashankSaumya123@users.noreply.github.com>
2018-08-17 06:03:14 +0000

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,16 @@
+//this is a cpp file
+// choosing library
+#include<iostream>
+using namespace std;
+//using the main command
+int main ()
+{
+//declaring the variables
+int a=2;
+char b = 'z';
+float c = 5.565;
+bool d = false;
+double e = 7.95;
+cout << " size of int is " << sizeof(a) << ", size of char is " << sizeof(b) << ", size of float is " << sizeof(c) << ", size of bool is " << sizeof(d) << ", size of double is " << sizeof(e);
+return 0;
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,28 @@
+//including library
+#include<iostream>
+// using namespace
+using namespace std;
+// using main command
+int main()
+{
+// defining 2 variables
+int a = 9;
+int b = 4;
+// add
+int sum = a+b;
+cout << " sum is " << sum;
+//subtract
+int sub = a-b;
+cout << ", difference is " << sub;
+//multiply
+int mul = a*b;
+cout << ", multiplication is " << mul;
+//divide
+int div = a/b;
+cout << ", quotient is " << div;
+// remainder
+int mod = a % b;
+cout << ", remainder is " << mod;
+//terminating the program
+return 0;
+}
