d6a1afeac8126e92b71cdc9b2dd4568232fdfc41
ShashankSaumya123 <42140139+ShashankSaumya123@users.noreply.github.com>
2018-08-31 01:37:26 +0530

    Add files via upload

---
+++ lab5_q25.cpp
@@ -0,0 +1,14 @@
+//to print all even numbers from 1 to 100
+// include library
+#include<iostream>
+using namespace std;
+int main()
+{
+	//defining varaiable
+	int i = 0;
+	//starting loop
+	while (i<=98)
+	{i = i+2;
+	cout << i << endl;}
+return 0;
+}
